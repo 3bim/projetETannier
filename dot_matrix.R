0222bdc82eba432c5d5b1c12bb848176a54c8c05
tmorealdeb <theophile.moreal-de-brevans@insa-lyon.fr>
2019-04-01 14:02:20 +0200

    debuger

--- dot_matrix.R
+++ dot_matrix.R
@@ -5,4 +5,14 @@ plot(c(),c(),xlim=c(1,4681143),ylim=c(1,4890491))
 
 segments(d$V7,d$V9,d$V8,d$V10)
 
+d2<-d
+liste=c()
+for (i in seq(length(d2[,1])-1)) {
+  
+  d[(d[i,7]==d[,7] & d[i,8]==d[,8]) | (d[i,9]==d[,9] & d[i,10]==d[,10]),]
+  
+  
+  d2<-d2[-liste,]
+  liste=c()
+}
 
