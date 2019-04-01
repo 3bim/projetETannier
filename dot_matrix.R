a306b3f9dda6dd641151088662b5dd5323585e1b
tmorealdeb <theophile.moreal-de-brevans@insa-lyon.fr>
2019-04-01 11:09:39 +0200

    affichage

--- dot_matrix.R
+++ dot_matrix.R
@@ -3,7 +3,7 @@ d<-d[d$V2=="NC_003143.1"]
 
 plot(c(),c(),xlim=c(1,4681143),ylim=c(1,4890491))
 
-segments(d$V7,d$V8,d$V9,d$V10)
+segments(d$V7,d$V9,d$V8,d$V10)
 
 min(d$V8-d$V7)
 print(summary(d$V2))
