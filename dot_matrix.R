a86d1b6e30e0098f2ad91dd1ad66d8ca48aa1070
tmorealdeb <theophile.moreal-de-brevans@insa-lyon.fr>
2019-04-01 11:19:38 +0200

    debuger

--- dot_matrix.R
+++ dot_matrix.R
@@ -1,9 +1,8 @@
-d<-read.table("A3GD8JZJ114-Alignment-HitTable.csv",sep=',',h=F)
-d<-d[d$V2=="NC_003143.1"]
+d<-read.table("projetETannier/A3GD8JZJ114-Alignment-HitTable.csv",sep=',',h=F)
+d<-d[d$V2=="NC_003143.1",]
 
 plot(c(),c(),xlim=c(1,4681143),ylim=c(1,4890491))
 
 segments(d$V7,d$V9,d$V8,d$V10)
 
-min(d$V8-d$V7)
-print(summary(d$V2))
+
