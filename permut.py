38253ebea3e2868c8e4baa51b41801d5c29253a0
Galaxy User <galaxy@localhost.(none)>
2019-04-22 15:52:01 +0200

    q2-3-4

--- permut.py
+++ permut.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 def nb_adjacences(permutation) :
 	a=0
 	for i in range(len(permutation)-1) :
@@ -8,7 +11,43 @@ def nb_adjacences(permutation) :
 	if permutation[len(permutation)-1]==max(permutation) :
 		a+=1
 	return a
-	
+
+def permut_1_inv(permutation) :
+	inv=list()
+	nb=nb_adjacences(permutation)
+	for i in range(len(permutation)-1) :
+		for j in range(i+1,len(permutation)) :
+			p=permutation[:i]+permutation[i:j+1][::-1]+permutation[j+1:]
+			if nb_adjacences(p)>nb :
+				inv.append(p)
+	return inv
+
+def min_inv(perm, nb=0) :
+	if nb_adjacences(perm)==len(perm)+1 :
+		return nb
+	else :
+		perm1=permut_1_inv(perm)
+		if perm1 :
+			nb_min=[]
+			for p in perm1 :
+				nb_min.append(min_inv(p,nb+1))
+			return min(nb_min)
+		return(float("inf"))
+
+def echantillon(n) :
+	ascii=string.ascii_uppercase[:n]
+	a=''
+	for _ in range(n) :
+		c=random.choice(ascii)
+		ascii=ascii.replace(c,'')
+		a+=c
+	return a
+
 if __name__=="__main__" :
-	permut="ABCD"
-	print(nb_adjacences(permut))
+	l=[]
+	for __ in range(10) :
+		for _ in range(100) :
+			r=min_inv(echantillon(7))
+			if r!=float('inf') :
+				l.append(r)
+		print float(sum(l))/float(len(l))
